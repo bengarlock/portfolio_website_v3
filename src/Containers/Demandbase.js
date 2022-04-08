5b5ff821221292ff6af3b819140e22e5258ea177
bengarlock <ben.garlock@gmail.com>
2022-04-08 10:43:10 -0400

    Updated demandbase tag

--- src/Containers/Demandbase.js
+++ src/Containers/Demandbase.js
@@ -5,15 +5,10 @@ class Demandbase extends React.Component {
 
     renderDemandbase = () => {
         return (
-            (function(d,b,a,s,e){
-                var t = b.createElement(a),
-                    fs = b.getElementsByTagName(a)[0];
-                    t.async = 1;
-                    t.id = e;
-                    t.src = s;
-                    fs.parentNode.insertBefore(t,fs);
-            })
-            (window,document,'script','https://tag.demandbase.com/edfc31da3c22de40.min.js','demandbase_js_lib'))
+            (function(d,b,a,s,e){var t=b.createElement(a),
+                fs=b.getElementsByTagName(a)[0];t.async=1;t.id=e;t.src=s;
+                fs.parentNode.insertBefore(t,fs);})
+            (window,document,'script','https://tag.demandbase.com/edfc31da3c22de40.min.js','demandbase_js_lib');
     }
 
     render() {
@@ -22,7 +17,6 @@ class Demandbase extends React.Component {
                 <h1>Welcome</h1>
                 <img src="https://alchemyinsurance.com/wp-content/uploads/2018/06/lightbulb.jpg" alt="light-bulb" />
                 <div>{this.renderDemandbase()}</div>
-                <iframe data-uri={"http://localhost:3000/demandbase"}></iframe>
             </div>
         )
     }
